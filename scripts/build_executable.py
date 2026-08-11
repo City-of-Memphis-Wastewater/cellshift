bfc773a34fa556a98b08a925826127f11a795d8c
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-08-10 19:27:30 -0500

    change consumption of the run_build_executable() function from maxson_build_utils

--- scripts/build_executable.py
+++ scripts/build_executable.py
@@ -8,7 +8,7 @@ Builds the standalone binary using maxson_build_utils.
 from __future__ import annotations
 
 from cellshift._version import __version__
-from cellshift.context import APP_NAME_PRETTY, SRC_FOLDER_NAME
+from cellshift.context import SRC_FOLDER_NAME
 from cellshift.paths import get_icns_icon, get_ico_icon, get_png_icon
 from maxson_build_utils.build_executable import run_build_executable
 
@@ -16,7 +16,6 @@ if __name__ == "__main__":
     run_build_executable(
         src_folder_name=SRC_FOLDER_NAME,
         version=__version__,
-        app_name_pretty=APP_NAME_PRETTY,
         icon_png_path=get_png_icon(),
         icon_ico_path=get_ico_icon(),
         icon_icns_path=get_icns_icon(),
